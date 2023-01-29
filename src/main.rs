1b7840c8680e778ca2d410239546f7d40e925c1a
Hung Nguyen <hung.nguyen@galaxyfinx.com>
2023-01-29 20:33:47 +0700

    Adding Guessing_game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number: u16 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {guess}");
+        let guess: u16 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(error) => {
+                println!("Parsing string error: {}", error.to_string());
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
